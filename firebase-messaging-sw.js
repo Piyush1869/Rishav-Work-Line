c83c449d34dc6e961fd1afbce5d6f5f529e7d3e5
Rishav Anand <102930248+Piyush1869@users.noreply.github.com>
2026-04-02 01:06:35 +0530

    Add files via upload

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,27 @@
+importScripts('https://www.gstatic.com/firebasejs/10.8.1/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.8.1/firebase-messaging-compat.js');
+
+// Initialize the Firebase app in the service worker
+firebase.initializeApp({
+    apiKey: "AIzaSyC3QMu8G5Q-1Fi8AoB2i3NtlusqjRbFVGg",
+    authDomain: "rishav-77936.firebaseapp.com",
+    projectId: "rishav-77936",
+    storageBucket: "rishav-77936.firebasestorage.app",
+    messagingSenderId: "568102781814",
+    appId: "1:568102781814:web:a7ba6f41ca70c6498b3057"
+});
+
+const messaging = firebase.messaging();
+
+// Handle background messages
+messaging.onBackgroundMessage((payload) => {
+    console.log('[firebase-messaging-sw.js] Received background message ', payload);
+    const notificationTitle = payload.notification.title;
+    const notificationOptions = {
+        body: payload.notification.body,
+        icon: payload.notification.image || 'https://via.placeholder.com/150',
+        requireInteraction: true // Keeps notification on screen until clicked
+    };
+
+    self.registration.showNotification(notificationTitle, notificationOptions);
+});
